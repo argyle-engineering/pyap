b005cf1ede74d4135a63d33db2081ef7d61a8aba
Lukas Melninkas <lukas.melninkas@argyle.com>
2024-04-25 21:22:26 +0300

    Detect country before postal code

--- tests/test_parser_us.py
+++ tests/test_parser_us.py
@@ -475,8 +475,10 @@ def test_full_street_positive(input, expected):
     "input,expected",
     [
         # positive assertions
+        ("2755 CARPENTER RD SUITE 1W\nANN ARBOR, MI, US, 48108", True),
         ("P.O. BOX 10323 PH (205) 595-3511\nBIRMINGHAM, AL 35202", True),
         ("25 HARBOR PARK DRIVE\nPORT WASHINGTON\nNY 11050", True),
+        ("222 W. Las Colinas Blvd\nSuite 900N\nIrving, Texas, USA 75039-5421", True),
         ("1100 VIRGINIA DR\nFORT WASHINGTON, PA, 19034", True),
         ("3602 HIGHPOINT\nSAN ANTONIO TX78217", True),
         ("8025 BLACK HORSE\nSTE 300\nPLEASANTVILLE NJ 08232", True),
